d670ce548dc0ea3a4877b690c170faa8a479917b
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-08 17:34:07 +0100

    Remove bc margin-bottom

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,5 +1,5 @@
-<nav style="--bs-breadcrumb-divider: '{{ $divider }}'; margin-bottom: 0;" aria-label="breadcrumb">
-    <ol class="breadcrumb">
+<nav style="--bs-breadcrumb-divider: '{{ $divider }}';" aria-label="breadcrumb">
+    <ol class="breadcrumb" style="margin-bottom: 0;">
         @foreach ($items as $index => $item)
             <li class="breadcrumb-item {{ $loop->last ? 'active' : '' }}">
                 @if ($loop->last || ! $item['uri'])
