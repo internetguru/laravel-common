f3e3c7c0bc717949e7d3b9f0c056656a2ca8b530
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-08 17:31:19 +0100

    Add messages styles

--- resources/views/components/messages.blade.php
+++ resources/views/components/messages.blade.php
@@ -1,24 +1,27 @@
-<div class="toast-wrapper" style="
-    z-index: 1050;
-    --bs-success-rgb: 38, 50, 56;
-">
-    <div class="toast-container">
-        @if(isset($errors) && $errors->any())
-            @foreach($errors->all() as $error)
-                <x-ig::message type="danger" message="{!! $error !!}"/>
-            @endforeach
-        @endif
-        @if(session('success', false))
-            @php
-            $data = session('success');
-            if (!is_array($data)) {
-                $data = [$data];
-            }
-            @endphp
-            @foreach ($data as $msg)
-                <x-ig::message type="success" message="{!! $msg !!}"/>
-            @endforeach
-        @endif
+ <div class="messages-wrapper">
+    <div>
+        <div class="toast-wrapper" style="
+            z-index: 1050;
+            --bs-success-rgb: 38, 50, 56;
+        ">
+            <div class="toast-container">
+                @if(isset($errors) && $errors->any())
+                    @foreach($errors->all() as $error)
+                        <x-ig::message type="danger" message="{!! $error !!}"/>
+                    @endforeach
+                @endif
+                @if(session('success', false))
+                    @php
+                    $data = session('success');
+                    if (!is_array($data)) {
+                        $data = [$data];
+                    }
+                    @endphp
+                    @foreach ($data as $msg)
+                        <x-ig::message type="success" message="{!! $msg !!}"/>
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </div>
 </div>
-
