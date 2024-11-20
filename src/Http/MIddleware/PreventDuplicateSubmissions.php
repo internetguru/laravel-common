e090d9f7b48ba197f6dfac38fbe5499b75adfa22
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-20 15:37:56 +0100

    Add PreventDuplicateSubmissions

---
+++ src/Http/MIddleware/PreventDuplicateSubmissions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace InternetGuru\LaravelCommon\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class PreventDuplicateSubmissions
+{
+    public function handle(Request $request, Closure $next): mixed
+    {
+        if ($request->isMethod('post')) {
+            // Generate a unique key based on the request ignoring the g-recaptcha-response field
+            $requestKey = sha1($request->ip() . '|' . $request->path() . '|' . serialize($request->except('g-recaptcha-response')));
+
+            // Check if the request has already been processed
+            if (cache()->has($requestKey)) {
+                // Duplicate request detected
+                return back()->withInput()->withErrors([
+                    'error' => 'Duplicate submission detected. Please wait before resubmitting.',
+                ]);
+            }
+
+            // Store the key in cache with a TTL
+            cache()->put($requestKey, true, now()->addMinutes(1));
+        }
+
+        return $next($request);
+    }
+}
