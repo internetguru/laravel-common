40dd55bb650e5aa494ba3176efc31ffff8244843
petrzpav <pavel.petrzela@internetguru.cz>
2025-02-03 06:13:52 +0100

    Update demo-info warning

--- resources/views/components/demo-info.blade.php
+++ resources/views/components/demo-info.blade.php
@@ -1,6 +1,6 @@
 @if(config('app.demo'))
     <div class="container-fluid alert alert-warning mb-0 rounded-0">
-        <p class="my-3">
+        <p class="my-0">
             {!! Str::inlineMarkdown(__('ig-common::messages.demo.warning')) !!}
         </div>
     </div>
