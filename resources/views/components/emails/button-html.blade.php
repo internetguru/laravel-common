d66d8817e3c81d48f3968346981bed2128ea0de2
petrzpav <pavel.petrzela@internetguru.cz>
2025-03-13 16:13:05 +0100

    Add email buttons usp

--- resources/views/components/emails/button-html.blade.php
+++ resources/views/components/emails/button-html.blade.php
@@ -2,4 +2,8 @@
     'link',
 ])
 
+@php
+    $link = strpos($link, '?') === false ? $link . '?usp=button' : $link . '&usp=button';
+@endphp
+
 <p style="text-align: center"><a href="{{ $link }}" class="button button-primary" target="_blank" rel="noopener">{{ $slot }}</a></p>
