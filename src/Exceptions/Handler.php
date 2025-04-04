0f746cb63f899fc445bcc045ea4d7840db8a25c5
petrzpav <pavel.petrzela@internetguru.cz>
2025-04-04 07:45:54 +0200

    Fix json error handling and back codes

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace InternetGuru\LaravelCommon\Exceptions;
 
+use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
@@ -13,22 +14,68 @@ class Handler extends ExceptionHandler
         $this->renderable(function (Throwable $e, $request) {
             $statusCode = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
 
-            if (! in_array($statusCode, [401, 402, 403, 404, 419, 429, 500, 503])) {
-                return response()->view('ig-common::layouts.base', [
-                    'exception' => $e,
-                    'view' => 'layouts.empty',
-                    'title' => "$statusCode " . __('ig-common::errors.unknown'),
-                    'description' => __('ig-common::errors.unknown_message'),
-                ], $statusCode);
+            // connection error from remote server, e.g. dns not resolved or timeout
+            if ($e instanceof ConnectException) {
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => __('ig-common::errors.connection_error')], 500);
+                }
+
+                return $this->back()->withErrors(__('ig-common::errors.connection_error'));
             }
 
-            return response()->view('ig-common::layouts.base', [
-                'exception' => $e,
-                'view' => 'layouts.empty',
-                'title' => "$statusCode " . __('ig-common::errors.' . $statusCode),
-                'description' => __('ig-common::errors.' . $statusCode . '_message'),
-            ], $statusCode);
-        });
+            // throttle handling
+            if ($statusCode == 429) {
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => __('ig-common::errors.ratelimit')], 429);
+                }
 
+                return $this->back()->withErrors([__('ig-common::errors.ratelimit')]);
+            }
+
+            // expired session
+            if ($statusCode == 419) {
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => __('ig-common::errors.session_expired')], 419);
+                }
+
+                return $this->back()->withErrors(__('ig-common::errors.session_expired'));
+            }
+
+            // global error
+            if ($request->expectsJson()) {
+                return response()->json(['message' => $e->getMessage()], $statusCode);
+            }
+
+            if (! in_array($statusCode, [401, 402, 403, 404, 419, 429, 500, 503])) {
+                return response()->view(
+                    'ig-common::layouts.base',
+                    [
+                        'exception' => $e,
+                        'view' => 'layouts.empty',
+                        'title' => "$statusCode " . __('ig-common::errors.unknown'),
+                        'description' => __('ig-common::errors.unknown_message'),
+                    ],
+                    $statusCode,
+                );
+            }
+
+            return response()->view(
+                'ig-common::layouts.base',
+                [
+                    'exception' => $e,
+                    'view' => 'layouts.empty',
+                    'title' => "$statusCode " . __('ig-common::errors.' . $statusCode),
+                    'description' => __('ig-common::errors.' . $statusCode . '_message'),
+                ],
+                $statusCode,
+            );
+        });
+    }
+
+    private function back()
+    {
+        return session('prevPage')
+            ? redirect(session('prevPage'))->withInput()
+            : redirect()->back()->withInput();
     }
 }
