03af02d38c1828c61fcb537bd1fe93b2973526a0
petrzpav <pavel.petrzela@internetguru.cz>
2025-04-07 07:11:35 +0200

    Update error page index

--- lang/cs/layouts.php
+++ lang/cs/layouts.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'i18n.index.description' => 'Rozcestník.',
+    'i18n.index.description' => ' ',
     'i18n.index.title' => 'Testovací stránka pro překlady',
     'i18n.missing-all.description' => 'Chybí všechny překlady.',
     'i18n.missing-all.title' => 'Testovací stránka pro překlady',
@@ -13,7 +13,7 @@ return [
     'i18n.complete.description' => 'Kompletní překlady.',
     'i18n.complete.title' => 'Testovací stránka pro překlady',
 
-    'errors.index.description' => 'Rozcestník.',
-    'errors.index.title' => 'Testovací stránka pro chyby.',
+    'errors.index.description' => ' ',
+    'errors.index.title' => 'Testovací stránka pro chyby',
 
 ];
--- lang/en/layouts.php
+++ lang/en/layouts.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'i18n.index.description' => 'Index page.',
+    'i18n.index.description' => ' ',
     'i18n.index.title' => 'Translation Test Page',
     'i18n.missing-all.description' => 'All translations are missing.',
     'i18n.missing-all.title' => 'Translation Test Page',
@@ -13,7 +13,7 @@ return [
     'i18n.complete.description' => 'Complete translations.',
     'i18n.complete.title' => 'Translation Test Page',
 
-    'errors.index.description' => 'Index page.',
-    'errors.index.title' => 'Error Test Pages.',
+    'errors.index.description' => ' ',
+    'errors.index.title' => 'Error Test Pages',
 
 ];
