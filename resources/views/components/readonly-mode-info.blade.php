a7468182b7d51307b81f028c36a897b8abb1b98e
petrzpav <pavel.petrzela@internetguru.cz>
2025-06-25 12:56:17 +0200

    Update readonly info style

--- resources/views/components/readonly-mode-info.blade.php
+++ resources/views/components/readonly-mode-info.blade.php
@@ -1,7 +1,6 @@
 @if (config('app.readonly', false))
-    <div class="alert alert-warning">
-        <p>
-            <i class="fa fa-exclamation-triangle"></i>
+    <div class="container-fluid alert alert-warning mb-0 rounded-0" data-testid="readonly-mode-info">
+        <p class="my-0">
             {!! \Illuminate\Support\Str::inlineMarkdown(__('ig-common::layouts.readonly_mode')) !!}
         </p>
     </div>
