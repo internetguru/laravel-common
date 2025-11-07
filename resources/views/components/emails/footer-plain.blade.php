5be729378cc7f50363977fd074208fd10eb4db0e
George <george@internetguru.io>
2025-11-07 22:56:03 +0100

    Add IP title to email footer

--- resources/views/components/emails/footer-plain.blade.php
+++ resources/views/components/emails/footer-plain.blade.php
@@ -2,4 +2,4 @@
 -- 
 @lang('ig-common::layouts.provider', ['link' => __('ig-common::layouts.provider.www'), 'year' => date('Y')])
 
-{{ $ip }}: @if($timezone)({{ $timezone }})@endif
+IP: {{ $ip }} @if($timezone)({{ $timezone }})@endif
