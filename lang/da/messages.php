9870ae063d9722cca0783827c31434f2c80c5bd9
petrzpav <pavel.petrzela@internetguru.cz>
2025-12-02 12:09:12 +0100

    Update readme and tests, clean translations

--- lang/da/messages.php
+++ lang/da/messages.php
@@ -14,5 +14,4 @@ return [
     'email.signature' => 'Team :company',
     'email.trouble' => 'Hvis du har problemer med at klikke på knappen, klik på URL\'en nedenfor, eller kopier den og indsæt den i din browser.',
 
-    'recaptcha' => 'Google kunne ikke bekræfte, at du er et menneske. Prøv at opdatere dit system eller din browser, brug en anden browser eller enhed, eller log ind.',
 ];
