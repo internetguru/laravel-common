0af4ef9ed185cc743537343a93d8aaa1dc49c115
petrzepav <pavel.petrzela@internetguru.cz>
2026-03-23 08:28:01 -0700

    Limit assoc history chars to 20

--- resources/views/components/association-history.blade.php
+++ resources/views/components/association-history.blade.php
@@ -6,8 +6,8 @@
                 <strong>{{ $history->author?->name ?? __('ig-common::messages.association_history.guest') }}</strong>
                 @lang('ig-common::messages.association_history.edited') <em>{{ $history->column_name }}</em>
                 @if (! $history->is_complex)
-                    @lang('ig-common::messages.association_history.from') <samp>{{ $history->column_prev_value ?? '–' }}</samp>
-                    @lang('ig-common::messages.association_history.to') <samp>{{ $history->new_value ?? '–' }}</samp>
+                    @lang('ig-common::messages.association_history.from') <samp title="{{ $history->column_prev_value ?? '–' }}">{{ Str::limit($history->column_prev_value ?? '–', 20) }}</samp>
+                    @lang('ig-common::messages.association_history.to') <samp title="{{ $history->new_value ?? '–' }}">{{ Str::limit($history->new_value ?? '–', 20) }}</samp>
                 @endif
             </li>
         @endforeach
