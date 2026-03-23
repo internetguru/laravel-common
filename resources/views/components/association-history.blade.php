3fd4bc70705e975c36cfd4b0ed9102235d7c2b20
petrzepav <pavel.petrzela@internetguru.cz>
2026-03-23 07:25:29 -0700

    Allow author id to be nullable ~ guest

--- resources/views/components/association-history.blade.php
+++ resources/views/components/association-history.blade.php
@@ -3,7 +3,7 @@
         @foreach ($histories as $history)
             <li class="mb-1">
                 <span class="text-muted">{{ $history->created_at->toDisplayTimezone()->dateTimeForHumans() }}</span>
-                <strong>{{ $history->author?->name ?? '–' }}</strong>
+                <strong>{{ $history->author?->name ?? __('ig-common::messages.association_history.guest') }}</strong>
                 @lang('ig-common::messages.association_history.edited') <em>{{ $history->column_name }}</em>
                 @if (! $history->is_complex)
                     @lang('ig-common::messages.association_history.from') <samp>{{ $history->column_prev_value ?? '–' }}</samp>
