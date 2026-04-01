7a05a3d6643fc27a4ef3759e9fb55216b15ad316
petrzepav <pavel.petrzela@internetguru.cz>
2026-04-01 11:31:31 -0700

    Change samp to code in notification footer

--- resources/views/components/emails/footer-html.blade.php
+++ resources/views/components/emails/footer-html.blade.php
@@ -10,14 +10,14 @@
 <hr />
 <p>
     @if ($refNumber)
-        @lang('ig-common::layouts.email.reference', ['ref' => '<samp>' . $refNumber . '</samp>'])<br />
+        @lang('ig-common::layouts.email.reference', ['ref' => '<code>' . $refNumber . '</code>'])<br />
     @endif
     @if ($noreplyMessage)
         {{ $noreplyMessage }}<br />
     @endif
     @lang('ig-common::layouts.email.generated-at', ['url' => $domainLink])<br />
-    @lang('ig-common::layouts.email.generator', ['generator' => '<samp>' . InternetGuru\LaravelCommon\Support\Helpers::getAppInfo() . '</samp>'])<br />
-    @lang('ig-common::layouts.email.requested-from', ['ip' => '<samp>' . $ip . '</samp>', 'timezone' => '<samp>' . ($timezone ?? 'n/a') . '</samp>'])
+    @lang('ig-common::layouts.email.generator', ['generator' => '<code>' . InternetGuru\LaravelCommon\Support\Helpers::getAppInfo() . '</code>'])<br />
+    @lang('ig-common::layouts.email.requested-from', ['ip' => '<code>' . $ip . '</code>', 'timezone' => '<code>' . ($timezone ?? 'n/a') . '</code>'])
 </p>
 <p>
     @lang('ig-common::layouts.provider', ['link' => $providerLink, 'year' => date('Y')])
