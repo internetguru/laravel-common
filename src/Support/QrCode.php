2f6a0bdb8a1ac0c97296fd6bff457e81bb02e26e
petrzpav <pavel.petrzela@internetguru.cz>
2026-08-14 07:38:08 +0200

    Truncate qr code log url to prevent data too big execption

--- src/Support/QrCode.php
+++ src/Support/QrCode.php
@@ -9,13 +9,19 @@ use BaconQrCode\Writer;
 
 class QrCode
 {
+    /** Longest content that still renders as a reasonably scannable QR code. */
+    public const MAX_LENGTH = 512;
+
     /**
      * Render the given content as an inline SVG QR code.
      *
+     * Content longer than self::MAX_LENGTH is truncated, as the encoder throws on oversized data.
      * The XML prolog is stripped so the result can be embedded directly in HTML.
      */
     public static function svg(string $content, int $size = 240, int $margin = 1): string
     {
+        $content = mb_strcut($content, 0, self::MAX_LENGTH);
+
         $writer = new Writer(new ImageRenderer(
             new RendererStyle($size, $margin),
             new SvgImageBackEnd
